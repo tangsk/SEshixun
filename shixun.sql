a3fbbbb7ac724701f16f76a376bd2543a49705b4
tangsk <40078060+tangsk@users.noreply.github.com>
2019-08-18 19:53:30 +0800

    Update shixun.sql

--- shixun.sql
+++ shixun.sql
@@ -9,7 +9,6 @@ CREATE  TABLE IF NOT EXISTS t_book (
   `author`        VARCHAR(110) NOT NULL,
   `publisher` 	  VARCHAR(110) NOT NULL,
   `publishtime`   VARCHAR(110) NOT NULL,
-  `ISBN`          INT(13) NOT NULL,
   PRIMARY KEY (`number`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
@@ -40,4 +39,4 @@ CREATE TABLE IF NOT EXISTS t_user (
   PRIMARY KEY (`uid`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
-INSERT  INTO `t_book`(`number`,`title`,`author`,`publisher`,`publishtime`,`ISBN`)  VALUES (12345678,'theroom','tangsk','scut','2019818',4841804445);
+INSERT  INTO `t_book`(`number`,`title`,`author`,`publisher`,`publishtime`)  VALUES (12345678,'theroom','tangsk','scut','20190818');
