72e554d70a1fcbd6b8aa41b85320be91fa7d4cb2
tangsk <40078060+tangsk@users.noreply.github.com>
2019-08-18 20:23:56 +0800

    Update shixun.sql

--- shixun.sql
+++ shixun.sql
@@ -17,25 +17,25 @@ CREATE  TABLE IF NOT EXISTS t_book (
 /*Table structure for table `t_borrow` */
 
 CREATE TABLE IF NOT EXISTS t_borrow (
-  `borrowID`      INT(12) NOT NULL AUTO_INCREMENT ,
-  `number`        INT(30) NOT NULL ,
+  `borrowID`      INT(11) NOT NULL AUTO_INCREMENT ,
+  `number`        INT(12) NOT NULL ,
   `title`         VARCHAR(110) NOT NULL,
   `author`        VARCHAR(110) NOT NULL,
-  `uid`           INT(12) NOT NULL  ,
+  `uid`           VARCHAR(12) NOT NULL  ,
   `name`          VARCHAR(20) NOT NULL,
-  `time`          INT(14) NOT NULL,
-  `srtime`        INT(14) NOT NULL,
+  `time`          VARCHAR(14) NOT NULL,
+  `srtime`        VARCHAR(14) NOT NULL,
   PRIMARY KEY (`borrowID`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
 /*Table structure for table `t_user` */
 
 CREATE TABLE IF NOT EXISTS t_user (
-  `uid`           INT(12) NOT NULL AUTO_INCREMENT ,
+  `uid`           VARCHAR(12) NOT NULL AUTO_INCREMENT ,
   `password`      VARCHAR(110) NOT NULL,
   `name`          VARCHAR(20) NOT NULL,
   `userkind`      VARCHAR(5) NOT NULL,
-  `phonenumber`   INT(14)  ,
+  `phonenumber`   VARCHAR(14)  ,
   `email`         VARCHAR(30)  ,
   PRIMARY KEY (`uid`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
