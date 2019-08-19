e79e6985e224cc8c633485a6ad793cb52cd5a376
tangsk <40078060+tangsk@users.noreply.github.com>
2019-08-19 20:26:31 +0800

    Update shixun.sql

--- shixun.sql
+++ shixun.sql
@@ -9,7 +9,7 @@ CREATE  TABLE IF NOT EXISTS t_book (
   `author`        VARCHAR(110) NOT NULL,
   `publisher` 	  VARCHAR(110) NOT NULL,
   `publishtime`   VARCHAR(110) NOT NULL,
-
+  `descri`        TEXT NOT NULL,
   PRIMARY KEY (`number`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
@@ -17,11 +17,11 @@ CREATE  TABLE IF NOT EXISTS t_book (
 /*Table structure for table `t_borrow` */
 
 CREATE TABLE IF NOT EXISTS t_borrow (
-  `borrowID`      INT(11) NOT NULL AUTO_INCREMENT ,
-  `number`        INT(12) NOT NULL ,
+  `borrowID`      INT(11) NOT NULL AUTO_INCREMENT,
+  `number`        INT(12) NOT NULL,
   `title`         VARCHAR(110) NOT NULL,
   `author`        VARCHAR(110) NOT NULL,
-  `uid`           VARCHAR(12) NOT NULL  ,
+  `uid`           VARCHAR(12) NOT NULL,
   `name`          VARCHAR(20) NOT NULL,
   `time`          VARCHAR(14) NOT NULL,
   `srtime`        VARCHAR(14) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS t_borrow (
 /*Table structure for table `t_user` */
 
 CREATE TABLE IF NOT EXISTS t_user (
-  `uid`           VARCHAR(12) NOT NULL AUTO_INCREMENT ,
+  `uid`           VARCHAR(12) NOT NULL ,
   `password`      VARCHAR(110) NOT NULL,
   `name`          VARCHAR(20) NOT NULL,
   `userkind`      VARCHAR(5) NOT NULL,
@@ -40,4 +40,4 @@ CREATE TABLE IF NOT EXISTS t_user (
   PRIMARY KEY (`uid`)
 ) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
-INSERT  INTO `t_book`(`number`,`title`,`author`,`publisher`,`publishtime`)  VALUES (12345678,'theroom','tangsk','scut','20190818');
+INSERT  INTO `t_book`(`number`,`title`,`author`,`publisher`,`publishtime`,`descri`)  VALUES (12345678,'theroom','tangsk','scut','20190818','the book of test');
