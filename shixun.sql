20cb6f83faee6e76168a9a455cebc1e42927377e
tangsk <40078060+tangsk@users.noreply.github.com>
2019-08-23 20:51:33 +0800

    Update shixun.sql

--- shixun.sql
+++ shixun.sql
@@ -1,43 +1,55 @@
-CREATE DATABASE IF NOT EXISTS shixun;
-USE shixun;
+CREATE DATABASE IF NOT EXISTS db_appapidemo;
 
-/*Table structure for table `t_book` */
+USE db_appapidemo;
 
-CREATE  TABLE IF NOT EXISTS t_book (
-  `number`        INT(12) NOT NULL AUTO_INCREMENT ,
-  `title`         VARCHAR(110) NOT NULL,
-  `author`        VARCHAR(110) NOT NULL,
-  `publisher` 	  VARCHAR(110) NOT NULL,
-  `publishtime`   VARCHAR(110) NOT NULL,
-  `descri`        TEXT NOT NULL,
-  PRIMARY KEY (`number`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
+CREATE  TABLE IF NOT EXISTS t_album(
+  `aid` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(110) NOT NULL,
+  `author` VARCHAR(110) NOT NULL,
+  `publisher` VARCHAR(110) NOT NULL,
+  `publishtime` VARCHAR(110) NOT NULL,
+  `num` INT(11) NOT NULL,
+  `time` VARCHAR(110) NOT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=INNODB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
 
-/*Table structure for table `t_borrow` */
 
-CREATE TABLE IF NOT EXISTS t_borrow (
-  `borrowID`      INT(11) NOT NULL AUTO_INCREMENT,
-  `number`        INT(12) NOT NULL,
-  `title`         VARCHAR(110) NOT NULL,
-  `author`        VARCHAR(110) NOT NULL,
-  `uid`           VARCHAR(12) NOT NULL,
-  `name`          VARCHAR(20) NOT NULL,
-  `time`          VARCHAR(14) NOT NULL,
-  `srtime`        VARCHAR(14) NOT NULL,
-  PRIMARY KEY (`borrowID`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
+CREATE TABLE IF NOT EXISTS t_borrowrecord (
+  `bid` INT(11) NOT NULL AUTO_INCREMENT,
+  `rid` INT(11) NOT NULL,
+  `raccount` VARCHAR(110) NOT NULL ,
+  `aid` INT(11) NOT NULL,
+  `sid` INT(11) NOT NULL,
+  `time` VARCHAR(110) NOT NULL,
+  `backtime` VARCHAR(110) NOT NULL,
+  `inttime` VARCHAR(110) NOT NULL ,
+  PRIMARY KEY (`bid`)
+) ENGINE=INNODB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
-/*Table structure for table `t_user` */
 
-CREATE TABLE IF NOT EXISTS t_user (
-  `uid`           VARCHAR(12) NOT NULL ,
-  `password`      VARCHAR(110) NOT NULL,
-  `name`          VARCHAR(20) NOT NULL,
-  `userkind`      VARCHAR(5) NOT NULL,
-  `phonenumber`   VARCHAR(14)  ,
-  `email`         VARCHAR(30)  ,
-  PRIMARY KEY (`uid`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
-INSERT  INTO `t_book`(`number`,`title`,`author`,`publisher`,`publishtime`,`descri`)  VALUES (12345678,'theroom','tangsk','scut','20190818','the book of test');
+
+CREATE TABLE IF NOT EXISTS t_reader (
+  `rid` INT(11) NOT NULL AUTO_INCREMENT,
+  `account` VARCHAR(110) NOT NULL,
+  `password` VARCHAR(110) NOT NULL,
+  `name` VARCHAR(110) NOT NULL,
+  `time` VARCHAR(110) NOT NULL,
+  `condi` INT(11) NOT NULL ,
+  `feedback` VARCHAR(140)  ,
+  `EMailAddr`VARCHAR(40),
+  `phoneNumber`VARCHAR(15),
+  PRIMARY KEY (`rid`)
+) ENGINE=INNODB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE  IF NOT EXISTS t_subalbum (
+  `sid` INT(11) NOT NULL AUTO_INCREMENT,
+  `aid` INT(11) NOT NULL,
+  `number` VARCHAR(110) NOT NULL,
+  `condi` INT(11) NOT NULL ,
+  `time` VARCHAR(110) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=INNODB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
